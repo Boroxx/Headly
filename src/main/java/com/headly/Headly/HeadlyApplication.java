a85f11452897ec31fb4993018d5e57d34713ebfd
Boroxx <tenelsen.boris@web.de>
2020-10-31 10:18:30 +0100

    Initial Setup

---
+++ src/main/java/com/headly/Headly/HeadlyApplication.java
@@ -0,0 +1,13 @@
+package com.headly.Headly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadlyApplication.class, args);
+	}
+
+}
