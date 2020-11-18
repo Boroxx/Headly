12f3626f032ffbb8e5f7b88aba33f58984098b27
Boroxx <tenelsen.boris@web.de>
2020-11-18 11:14:27 +0100

    added Transactional to admincontroller for bugfix

--- src/main/java/com/headly/Headly/controller/AdminController.java
+++ src/main/java/com/headly/Headly/controller/AdminController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,8 @@ public class AdminController {
     model.addAttribute("applications",applicationModelService.loadApplicationModelList());
     return "applicationoverview";
   }
+
+  @Transactional
   @GetMapping("/admin/{applicationid}")
   public ResponseEntity<byte[]> getPDF1(@PathVariable String applicationid) {
     HttpHeaders headers = new HttpHeaders();
