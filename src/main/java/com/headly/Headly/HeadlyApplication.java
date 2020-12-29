dc0e9d9ee228fe65d9c71981f67396d6bf3e569d
boris <tenelsen.boris@web.de>
2020-12-29 13:20:46 +0100

    deploy

--- src/main/java/com/headly/Headly/HeadlyApplication.java
+++ src/main/java/com/headly/Headly/HeadlyApplication.java
@@ -33,7 +33,7 @@ public class HeadlyApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+	/*
 
 			User applicant = User.builder().role("BEWERBER").city("moenchengladbach").firstname("Boris").lastname("Tenelsen").email("tenelsen.boris@web.de").housenumber("40")
 							.phonenumber("0151123456").street("Hoemenstrasse").zipcode("41199").password("dennis").build();
@@ -50,18 +50,40 @@ public class HeadlyApplication implements CommandLineRunner {
 
 			}
 
-
+		*/
 
 		Profession profession1 = new Profession(1,"Bauwesen");
 		Profession profession2 = new Profession(2,"Dienstleistung");
 		Profession profession3 = new Profession(3,"Elektro");
-		Profession profession4 = new Profession(4,"Gesundheit");
+		Profession profession4 = new Profession(4,"IT");
+		Profession profession5 = new Profession(5,"Kunst, Kultur und Gestaltung");
+		Profession profession6 = new Profession(6,"Landwirtschaft, Natur und Umwelt");
+		Profession profession7 = new Profession(7,"Metall, Maschinenbau");
+		Profession profession8 = new Profession(8,"Naturwissenschaften");
+		Profession profession9 = new Profession(9,"Produktion, Fertigung");
+		Profession profession10 = new Profession(10,"Soziales");
+		Profession profession11 = new Profession(11,"Technik und Technologie");
+		Profession profession12 = new Profession(12,"Verkehr und Logistik");
+		Profession profession13 = new Profession(13,"Wirtschaft und Verwaltung");
+
+
+
 		professionService.registerProfession(profession1);
 		professionService.registerProfession(profession2);
 		professionService.registerProfession(profession3);
 		professionService.registerProfession(profession4);
+		professionService.registerProfession(profession5);
+		professionService.registerProfession(profession6);
+		professionService.registerProfession(profession7);
+		professionService.registerProfession(profession8);
+		professionService.registerProfession(profession9);
+		professionService.registerProfession(profession10);
+		professionService.registerProfession(profession11);
+		professionService.registerProfession(profession12);
+		professionService.registerProfession(profession13);
 
 
+		/*
 
 		List<Jobpost> jobposts = new ArrayList<>();
 		for(int i= 1; i< 5; i++){
@@ -80,7 +102,7 @@ public class HeadlyApplication implements CommandLineRunner {
 		for (Jobpost jobpost : jobposts) {
 			postingService.registerPost(jobpost);
 		}
-
+	*/
 
 
 
