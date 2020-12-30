8abac2dd1b30046659a789e836060122032b0976
boris <tenelsen.boris@web.de>
2020-12-30 13:04:30 +0100

    removed alert, FIRST PROD DEPLOY with SSL

--- src/main/java/com/headly/Headly/HeadlyApplication.java
+++ src/main/java/com/headly/Headly/HeadlyApplication.java
@@ -34,7 +34,6 @@ public class HeadlyApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		/*
-
 			User applicant = User.builder().role("BEWERBER").city("moenchengladbach").firstname("Boris").lastname("Tenelsen").email("tenelsen.boris@web.de").housenumber("40")
 							.phonenumber("0151123456").street("Hoemenstrasse").zipcode("41199").password("dennis").build();
 			User company = User.builder().role("UNTERNEHMEN").city("moenchengladbach").companyname("headly").contactperson("Herr Tenelsen").email("test@test.de").housenumber("40")
@@ -50,7 +49,7 @@ public class HeadlyApplication implements CommandLineRunner {
 
 			}
 
-/*
+	*/
 
 		Profession profession1 = new Profession(1,"Bauwesen");
 		Profession profession2 = new Profession(2,"Dienstleistung");
