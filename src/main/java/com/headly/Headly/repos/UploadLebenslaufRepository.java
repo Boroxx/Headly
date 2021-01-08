def6df49ee47636a2cd38df85c1d503ad8413f8c
Boroxx <tenelsen.boris@web.de>
2021-01-08 14:08:00 +0100

    fixxed Bug

--- src/main/java/com/headly/Headly/repos/UploadLebenslaufRepository.java
+++ src/main/java/com/headly/Headly/repos/UploadLebenslaufRepository.java
@@ -2,7 +2,9 @@ package com.headly.Headly.repos;
 
 import com.headly.Headly.models.Lebenslauf;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface UploadLebenslaufRepository extends JpaRepository<Lebenslauf,Long> {
 
     Lebenslauf findByUserId(int id);
